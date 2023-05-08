bdaec9422ed71f5d2aaf0dcc154d63c5b176704b
Chloe Derigal <chloederigal@gmail.com>
2023-05-08 18:39:20 +0200

    modification explication en cours

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-
+//création du tableau d'objet des photos et des textes
 const slides = [
 	{
 	  "image": "slide1.jpg",
@@ -17,50 +17,60 @@ const slides = [
 	  "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
   ];
-  
+  // Déclaration et récuperation du DOM
   const sliderImg = document.querySelector('#slider_img');
-  const diapo = document.querySelector('#banner');
   const left = document.querySelector('.arrow_left');
   const right = document.querySelector('.arrow_right');
   const dots = document.querySelectorAll('.dot');
+  const tagLineVar = document.querySelector('#p');
   let slideIndex = 0;
   
 
-  //ici la fonction changeslide
+  //ici la fonction changeslide qui prends l'index en parametre et met a jour le contenu de la slide actuelle en fonction de l'index
   function changeSlide(index) {
+	//assignation de la variable index à slideIndex
 	slideIndex = index;
+
 	const slide = slides[slideIndex];
+	console.log(slideIndex);
+	//qui charge l 'image de la slide
 	sliderImg.src = `./assets/images/slideshow/${slide.image}`;
-	document.querySelector('#p').innerHTML = slide.tagLine;
+	//qui charge le texte de la slide
+	tagLineVar.innerHTML = slide.tagLine;
+	//mise à jour de la dot
 	updateDots();
   }
-  
+  //mise a jour des petits points en bas
   function updateDots() {
+	//pour chaque dots 
 	dots.forEach((dot, index) => {
 	  if (index === slideIndex) {
+		console.log(slideIndex);
 		dot.classList.add('dot_selected');
 	  } else {
 		dot.classList.remove('dot_selected');
 	  }
 	});
   }
-  
+  //gestionnaire d'evenement sur le bouton gauche
   left.addEventListener('click', () => {
 	let index = slides.indexOf(slides[slideIndex]) - 1;
+	console.log(slideIndex);
 	if (index < 0) {
 	  index = slides.length - 1;
 	}
 	changeSlide(index);
   });
-  
+  //gestionnaire d'eveement sur le bouton droit 
   right.addEventListener('click', () => {
 	let index = slides.indexOf(slides[slideIndex]) + 1;
+	console.log(slideIndex);
 	if (index > slides.length - 1) {
 	  index = 0;
 	}
 	changeSlide(index);
   });
-  
+  //gestionaire d'evenements sur les dots aussi on peux cliquer
   dots.forEach((dot, index) => {
 	dot.addEventListener('click', () => {
 	  changeSlide(index);
